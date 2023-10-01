b2433959302292ddb0daa3b749938e115f32795d
Brady Henderson <bradyhendu@gmail.com>
2023-10-01 19:08:09 -0400

    Adds Basic NavBar Component w/ Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,30 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            <h1> NAVBAR </h1>
+        <div class="navbar">
+                <div class="left">
+                    <b class="paw-plan"><span>paw-plan</span></b>
+                    <div class="buttons-group">
+                        <div class="menu-item">
+                            <div class="button-text">About</div>
+                        </div>
+                    <div class="menu-item">
+                        <div class="button-text">Contact</div>
+                    </div>
+                </div>
+            </div>
+            <div class="buttons-group1">
+                <div class="button">
+                    <div class="text-container">
+                        <div class="button-text">Log In</div>
+                    </div>
+                </div>
+                <div class="button1">
+                    <div class="text-container">
+                        <div class="button-text">Register</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
