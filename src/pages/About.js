2872412df311474166f2c3d15a3c5d9aab1d84aa
Brady Henderson <bradyhendu@gmail.com>
2023-10-02 11:59:16 -0400

    Creates Dummy About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const About = () => {
-
+    return (
+        <div>
+            <h1>
+                Paw-Plan is the Awesomest thing ever!
+            </h1>
+        </div>
+    );
 }
 
 export default About;
