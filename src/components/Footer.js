ac7b56d726829f104352605191c26fdc34f10511
Brady Henderson <bradyhendu@gmail.com>
2023-10-01 21:44:11 -0400

    Adds Footer Component and Styles

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div class="bottom">
+            <div class="companyname-202x">@ 2023 Paw-Plan. All rights reserved</div>
+        </div>
+    )
+}
+
+export default Footer;
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import Footer from "./Footer";
+
 const Home = () => {
     return (
         <div className="home-container">
             <Navbar />
             <Hero />
+            <Footer />
         </div>
     )
 }
