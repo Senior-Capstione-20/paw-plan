c55db8189db70304306c3220fdd5a327ba2e04ca
Brady Henderson <bradyhendu@gmail.com>
2023-10-01 22:15:45 -0400

    Adds Planning Component and Styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Footer from "./Footer";
+import Planning from "./Planning";
 
 const Home = () => {
     return (
         <div className="home-container">
             <Navbar />
             <Hero />
+            <Planning />
             <Footer />
         </div>
     )
---
+++ src/components/Planning.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Doghike from '../images/dog-hike.png';
+import Threedogs from '../images/3dogs.png';
+import DogVet from '../images/dog-vet.png';
+import DogHouse from '../images/dog-house.png';
+import DogPark from '../images/dog-park.png';
+
+const Planning = () => {
+    return (	
+  	    <div class="planning">
+    		<div class="plan-row">
+      			<img class="dog-hike" alt="" src={Doghike} />
+      			
+      			<div class="section-text">
+        				<div class="top">
+          					<b class="caption">Planning</b>
+          					<div class="secondary-headline">Easily make your schedule around your dog</div>
+        				</div>
+        				<div class="paragraph">Paw-Plan gives you the resources to keep you and others informed on everything dog related! Stop miscommunication today.</div>
+        				<div class="plan-buttons-group">
+          					<div class="plan-button">
+            						<div class="plan-text-container">
+              							<b class="plan-button-text">Register</b>
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+        		</div>
+        		<div class="planning-row1">
+          			<img class="three-dogs" alt="" src={Threedogs}/>
+          			<img class="dog-vet" alt="" src={DogVet}/>
+          			<img class="dog-park" alt="" src={DogPark}/>
+          			<img class="dog-house" alt="" src={DogHouse}/>
+          			
+        		</div>
+      	    </div>
+        );
+    }
+    export default Planning;
+
