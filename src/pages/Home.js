16afb1d67c429e816a7f705129b47bc0906f2da9
Brady Henderson <bradyhendu@gmail.com>
2023-10-02 11:34:03 -0400

    Moves home to a new folder pages, updates imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "./Navbar";
-import Hero from "./Hero";
-import Footer from "./Footer";
-import Planning from "./Planning";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+import Planning from "../components/Planning";
 
 const Home = () => {
     return (
