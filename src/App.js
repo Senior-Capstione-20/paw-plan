4617988a107432e07a15fe60436fc5037a99c9a1
Chelsea <Chelseamensah2@gmail.com>
2023-10-10 22:15:41 -0400

    created pet registration css file.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import About from './pages/About';  
 
+
+// import pet registration component
+
+
 function App() {
   return (
     <Router>
