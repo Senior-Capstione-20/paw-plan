30d20d69d902ec85eb0677ae9891a54875229c18
Brady Henderson <bradyhendu@gmail.com>
2023-10-30 16:31:47 -0400

    Preparing for About Page implementation

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>
-                Paw-Plan is the Awesomest thing ever!
-            </h1>
+           
         </div>
     );
 }
