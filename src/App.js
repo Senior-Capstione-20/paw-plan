2e600d973a825f4ed2e16cffe4a5a88c838eff50
Brady Henderson <bradyhendu@gmail.com>
2023-11-20 12:14:04 -0500

    Adds Profile route

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 import Home from './pages/Home'; 
 import Navbar from './components/Navbar';
 import About from './pages/About';  
+import Profile from './pages/Profile';
 import LogInPage from './pages/LogIn';
 import PetRegistration from './pages/PetRegistration';
 //  <Route path='/petregistration' element={<PetRegistration />} />
 
+
 import RegistrationPage from './pages/Registration';
 // import pet registration component
-
 
 function App() {
   return (
@@ -21,10 +22,9 @@ function App() {
         <Route exact path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/login' element={<LogInPage />} />
-        
         <Route path='/registration' element={<RegistrationPage />} />
         <Route path ='/petregistration' element={<PetRegistration />} />
-
+        <Route path ='/profile' element={<Profile />} />
 
       </Routes>
     </Router>
