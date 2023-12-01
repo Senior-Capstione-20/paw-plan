2caacf5a2867d905acd37b77d4639eb3b7aa162b
Brady Henderson <bradyhendu@gmail.com>
2023-12-01 11:56:47 -0500

    fixes spacing

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -43,6 +43,7 @@ const Login = () => {
 			setUser('');
 			setPassword('');
 			setSuccess(true);
+		
 		} catch (err) {
 			// in case of error, parse error message and display it
 			if (!err?.response) {
