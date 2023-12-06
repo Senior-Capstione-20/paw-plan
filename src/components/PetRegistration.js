bc55d70b72169583fab3a8e7c64ec380f5575941
Brady Henderson <bradyhendu@gmail.com>
2023-12-06 12:14:03 -0500

    Changes CSS for Pet Registration

--- src/components/PetRegistration.js
+++ src/components/PetRegistration.js
@@ -3,50 +3,42 @@ import "./PetRegistrationSection.css";
 
 function PetRegistration() {
   return (
-    <div className="PetRegistration">
-       <b className="register-pet">{`Register Pet `}</b>
-      <img className="gsp-1-icon" alt="" src="/gsp-1@2x.png" />
-      
-     
+    <div className="petregistration-wrapper">
+      <div className="PetRegistration">
+        
+        <b className="register-pet">{`Register Pet `}</b>
+        <img className="gsp-1-icon" alt="" src="/gsp-1@2x.png" />
+        <label  htmlFor="name"> Name</label>
+              <input
+              className="name"
+              type="text"
+              placeholder="Enter Name"
+              />
+              
 
+        <label  htmlFor="age"> Age</label>
+              <input
+              className="age"
+              type="text"
+              placeholder="Enter Age"
+        />
 
-<label  htmlFor="name"> Name</label>
-      <input
-       className="name"
-       type="text"
-       placeholder="Enter Name"
-       />
-      
+        <label  htmlFor="breed"> Breed</label>
+              <input
+                className="breed"
+                type="text"
+                placeholder="Enter Breed"
+        />
 
-      <label  htmlFor="age"> Age</label>
-      <input
-      className="age"
-      type="text"
-      placeholder="Enter Age"
-      />
-
-<label  htmlFor="breed"> Breed</label>
-      <input
-        className="breed"
-        type="text"
-        placeholder="Enter Breed"
-      />
-
-<label  htmlFor="weight"> Weight</label>
-      <input
-        className="weight"
-        type="text"
-        placeholder="Enter Weight"
-      />
-
-
-    
-
-
-
-
-      <button className="rectangle-button">Register</button>
-   
+        <label  htmlFor="weight"> Weight</label>
+              <input
+                className="weight"
+                type="text"
+                placeholder="Enter Weight"
+        />
+        <button className="rectangle-button">Register</button>
+          
+      </div>
     </div>
   );
 }
