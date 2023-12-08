ba0aeca0d0e69820e3085c2a4c7895163f8977f9
Brady Henderson <bradyhendu@gmail.com>
2023-12-08 15:36:24 -0500

    Initializes firebase

--- src/App.js
+++ src/App.js
@@ -9,10 +9,23 @@ import Profile from './pages/Profile';
 import LogInPage from './pages/LogIn';
 import PetRegistration from './pages/PetRegistration';
 import Dashboard from './pages/Dashboard';
-
+import {initializeApp} from 'firebase/app';
 
 import RegistrationPage from './pages/Registration';
 // import pet registration component
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCIfqQxDGMRiiVl_jSh6JSBpnzYtO_LyuY",
+  authDomain: "paw-plan-96d84.firebaseapp.com",
+  projectId: "paw-plan-96d84",
+  storageBucket: "paw-plan-96d84.appspot.com",
+  messagingSenderId: "680254872125",
+  appId: "1:680254872125:web:de1f95db2bd54bc3a06854",
+  measurementId: "G-WY7L3Y67DX"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
